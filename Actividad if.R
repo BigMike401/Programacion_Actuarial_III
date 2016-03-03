f54c396f292ee68d90199788b9815b6c5a2a60e5
BigMike401 <tigre4011@gmail.com>
2016-03-03 16:02:47 -0600

    Actividad if
    Subida de la actividad del if

---
+++ Actividad if.R
@@ -0,0 +1,11 @@
+x<-runif(1)
+x
+if(x>=0 && x<=0.2) {
+    "Quiubo"
+} else if(x>=0.2 && x<=0.5) {
+    "Qué onda"
+} else if(x>=0.5 && x<=0.9) {
+    "hola"
+} else if(x>=0.9 && x<=1) {
+    "Qué pedo güey"
+}
--- Mártes 01 de marzo del 2016 airquality.R
+++ Mártes 01 de marzo del 2016 airquality.R
@@ -20,3 +20,47 @@ x[["a",exact=F]]
 
 airquality
 
+y <- airquality
+dput(y,"airquality.R")
+nuevay <- dget(file="airquality.R")
+
+#Sacar NA de los valores de airquality
+head(airquality)
+datoscompletos <- airquality[complete.cases(airquality),]
+datoscompletos
+
+nrow(datoscompletos)
+dim(datoscompletos)
+?sum
+dim(datoscompletos)[1]
+sum(complete.cases(airquality))
+
+#Estructuras de control
+x <- 11
+if(x>3){
+    y<-10
+} else {
+    y<-5
+}
+y
+
+x<-2
+y<- if(x>3) {
+    10
+} else {
+    5
+}
+y
+
+?runif
+x<-runif(1)
+x
+if(x>=0 && x<=0.2) {
+    "Quiubo"
+} else if(x>=0.2 && x<=0.5) {
+    "Qué onda"
+} else if(x>=0.5 && x<=0.9) {
+    "hola"
+} else if(x>=0.9 && x<=1) {
+    "Qué pedo güey"
+}
